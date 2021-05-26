5f6d6d7200daf5d8245e09539d4c3302e1587836
DhairyaShah2008 <81810113+DhairyaShah2008@users.noreply.github.com>
2021-05-26 16:21:15 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,22 @@
+class stone{
+    constructor(x, y, width, height) {
+        var options = {
+                     
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+        this.image=loadImage("polygon.png")
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        rectMode(CENTER);
+        image(this.image,this.body.position.x-160,this.body.position.y-170,20,20)
+        pop();
+      }
+}
